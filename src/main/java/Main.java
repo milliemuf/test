5dd93df8a710fd0aa8705e37d03d874d62973299
milliemuf <milliemuf03@gmail.com>
2021-12-31 04:06:36 +0200

    tym

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import io.quarkus.runtime.Quarkus;
+
+public class Main {
+    public static void main(String[]args) {
+
+        //	Log.info("Starting quarkus.........");
+        Quarkus.run();
+
+    }
+}
---
+++ src/main/java/org/transformafrica/responses/Response.java
@@ -0,0 +1,21 @@
+package org.transformafrica.responses;
+
+public class Response {
+    private String description;
+    private Integer descriptionCode;
+
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Integer getDescriptionCode() {
+        return descriptionCode;
+    }
+    public void setDescriptionCode(Integer descriptionCode) {
+        this.descriptionCode = descriptionCode;
+    }
+
+}
